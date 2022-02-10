b9762d49804e96d95411332e7a965826ddfd0865
Damjan Stojanovski <damjan.stojanovski@haselt.com>
2022-02-10 14:23:45 +0100

    added new funcs

--- RestaurantInventoryManagment/Horoko.InventoryManagment.Services/Services/WorkableService.cs
+++ RestaurantInventoryManagment/Horoko.InventoryManagment.Services/Services/WorkableService.cs
@@ -1,7 +1,9 @@
-﻿using Horoko.InventoryManagment.DataBase.Models.Enums;
+﻿using Horoko.InventoryManagment.Database.Models;
+using Horoko.InventoryManagment.DataBase.Models.Enums;
 using Horoko.InventoryManagment.Services.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Horoko.InventoryManagment.Services.Services
@@ -10,7 +12,7 @@ namespace Horoko.InventoryManagment.Services.Services
     {
         public int GetArticlesSoldOnSpecificDate(int articleId, DateTime date)
         {
-            throw new NotImplementedException();
+           
         }
 
         public List<IngredientAmountAndSalesRecordJoined> GetDetailedView()
@@ -39,13 +41,23 @@ namespace Horoko.InventoryManagment.Services.Services
         {
             GetDataService ds = new GetDataService();
             var salesRecords = ds.GetSalesRecordData(Config.SalesRecordsFilePath);
-            return 0;
 
-            //return salesRecords.GroupBy(x => x.DateAndTimeOfOrder.Date, (date, listOfItems) =>
-            //{
-            //    var maxSale = listOfItems.Sum(i => i.Price * i.Amount);
-            //    return new { date, maxSale };
-            //}).OrderByDescending(x => x.maxSale).First().date;
+            var groupsByWeek = salesRecords.GroupBy(i => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(
+                                    i.DateAndTimeOfOrder.Date, CalendarWeekRule.FirstDay, DayOfWeek.Monday));
+
+            Dictionary<int, decimal> data = new Dictionary<int, decimal>();
+
+            decimal maxSales = 0;
+            foreach (var item in groupsByWeek)
+            {
+                for (int i = 0; i < item.Count(); i++)
+                {
+                    maxSales += (item.ElementAt(i).Amount * item.ElementAt(i).Price);
+                }
+                data.Add(item.Key, maxSales);
+            };
+
+            return data.OrderByDescending(x => x.Value).First().Key;
         }
     }
 }
